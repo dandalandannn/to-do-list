12584fe0313429d91e073e7e6ebbd3a546fbc73d
dandalandannn <danielgwen.domingo01@gmail.com>
2025-01-11 09:50:01 +0800

    linis pwede na sa tupad

--- script.js
+++ script.js
@@ -1,28 +1,79 @@
-const input = document.querySelector(".input");
-const dateBtn = document.querySelector(".date-btn");
-const toDoList = document.querySelector(".to-do-list");
+const taskInput = document.querySelector(".task-input");
+const dateInput = document.querySelector(".date-input");
+const checkboxes = document.querySelector(".checkboxes");
+const checkedCheckboxes = document.querySelector(".checked-checkboxes");
+const taskContainer = document.querySelector(".task-list-container");
+const finishedContainer = document.querySelector(".finished-list-container");
 
-const taskList = ["Sample"];
-const dueList = ["2025-01-10"];
+const date = new Date()
+const result = date.toISOString().split('T')[0]
 
-const addTask = (event) => {
-    event.preventDefault();
-    taskList.push(input.value);
-    dueList.push(dateBtn.value);
+const taskList = [];
+const dateList = [];
 
-    console.log(typeof event);
-    
-    input.value = "";
-    dateBtn.value = "";
-    renderList();
-}
+const fTaskList = [];
+const fDateList = [];
 
-const renderList = () => {
-    let HTML = "";
+const renderShit = () => {
+    let tempHTML = "";
+    let fTempHTML = "";
+
     for(let i = 0; i < taskList.length; i++){
-            
-        HTML += `<div class="tasks" id="${i}">${taskList[i]} <p class="due-date">${dueList[i]}</p>
-        </div> <div><input type="checkbox" class="checkbox" id="cb${i}" ></input></div>`;
+        tempHTML += 
+            `<div class="tasks">
+                <div>${taskList[i]}
+                    <p class="dates">${result === dateList[i]? `Today`: dateList[i]}</p>
+                </div>
+                <div>
+                    <input type="checkbox" class="checkboxes" onclick="handleFinished(${i})">
+                </div>
+            </div>`;
     }
-    toDoList.innerHTML = HTML;
+    for(let i = 0; i < fTaskList.length; i++){
+        fTempHTML +=
+            `<div class="tasks">
+                <div>${fTaskList[i]}
+                   <p class="dates">${result === fDateList[i]? `Today`: fDateList[i]}</p>
+                </div>
+                <div>
+                    <input type="checkbox" checked class="checked-checkboxes"  onclick="handleUnfinish(${i})">
+                </div>
+            </div>`;
+    }
+    taskContainer.innerHTML = tempHTML;
+    finishedContainer.innerHTML = fTempHTML;
 }
+
+const handleTaskInput = (e) => {
+    e.preventDefault();
+
+    taskList.push(taskInput.value);
+    dateList.push(dateInput.value);
+
+    renderShit();
+    taskInput.value = "";
+    dateInput.value = "";
+}
+const handleFinished = (num) => {
+    
+
+    fTaskList.push(taskList[num]);
+    fDateList.push(dateList[num]);
+
+    taskList.splice(num, 1);
+    dateList.splice(num, 1);
+    
+    renderShit();
+}
+const handleUnfinish = (num) => {
+    taskList.push(fTaskList[num]);
+    dateList.push(fDateList[num]);
+
+    fTaskList.splice(num, 1);
+    fDateList.splice(num, 1);
+    
+    renderShit();
+}
+
+renderShit();
+console.log("tangin");
